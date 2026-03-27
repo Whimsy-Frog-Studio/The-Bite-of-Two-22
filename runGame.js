f7a14642ae3a8d8dfc5510e60b90aba37d2785cc
hojacky07 <hojacky.jch07@gmail.com>
2026-03-27 15:59:56 -0400

    Added Inventory.js into index.html.

--- runGame.js
+++ runGame.js
@@ -1,24 +1,16 @@
 let player;
-let inventory_slot1;
-let inventory_slot2;
-let inventory_slot3;
-let inventory_slot4;
-let inventory_slot5;
-let inventory_slot6;
-let inventory_slot7;
-let inventory_slot8;
+let inventorySlots = [];
 
 function setup() {
   new Canvas(1920, 1080);
   player = new Player(0,0)
-  inventory_slot1 = new Inventory(1,0,1016);
-  inventory_slot2 = new Inventory(2,63,1016);
-  inventory_slot3 = new Inventory(3,126,1016);
-  inventory_slot4 = new Inventory(4,189,1016);
-  inventory_slot5 = new Inventory(5,252,1016);
-  inventory_slot6 = new Inventory(6,315,1016);
-  inventory_slot7 = new Inventory(7,378,1016);
-  inventory_slot8 = new Inventory(8,441,1016);
+  for (let i = 0; i < 8; i++)
+  {
+    let slotNum = i + 1;
+    let slotX = i * 63;
+
+    inventorySlots.push(new Inventory(slotNum, slotX, 1016))
+  }
 }
 
 function draw() {
