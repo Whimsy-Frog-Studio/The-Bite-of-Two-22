716af52a08c6ef59f13b59358abcaf3e17592724
hojacky07 <hojacky.jch07@gmail.com>
2026-04-23 22:40:41 -0400

    A fresh start for the bugged game, temporary branch, might become the new main.

--- runGame.js
+++ runGame.js
@@ -1,40 +1,12 @@
 let player;
-let inventorySlots = [];
 
 function setup() 
 {
   new Canvas(1528, 842);
-  player = new Player(764,421);
-
-  stageController = new StageController("RCC_First_Floor", width, height);
-  
-  player.SetStageController(stageController);
-
-  /* for (let i = 0; i < 8; i++)
-  {
-    let slotNum = i + 1;
-    let slotX = i * 47 + 764 - 178;
-
-    inventorySlots.push(new Inventory(slotNum, slotX, 784));
-  }
-  */
 }
 
 function draw() 
 {
-  clear();
-  player.Move();
-  player.UpdateSprite();
-  player.UpdateStageController();
-  
-  /* for (let i = 0; i < inventorySlots.length; i++) 
-  {
-    inventorySlots[i].UpdateSlot(inventorySlots);
-  }
-  */
 
-  fill(0);
-  text(player.targetVelY, 20, 40);
-  text(stageController.stage, 20, 20);
 }
 
