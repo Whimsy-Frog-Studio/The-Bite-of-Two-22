ee6755cb37612c677a07a922bb8a2eb1ee4fe8a7
hojacky07 <hojacky.jch07@gmail.com>
2026-04-27 01:16:04 -0400

    Added Colliders As Well As Player Sprites and a single dialogue to showcase work in progress.

--- runGame.js
+++ runGame.js
@@ -1,55 +1,114 @@
-// Player Variables
+// Game Variables
 let player;
 let playerXDirection;
 let playerYDirection;
 let lastXKey = '';
 let lastYKey = '';
+let dialogueTimer = 0;
 
-// Sprite Costume Variables
-let idleAniK;
+// Kyla The Player
+let idleDownAniK;
+let idleUpAniK;
 let walkLAniK;
 let walkRAniK;
 let walkUAniK;
 let walkDAniK;
+let dialogueKyla;
+let showDialogueKyla = false;
+
+//NPC Stan
+let stanIdleAni;
+let showDialogueStan = false;
+let dialogueStan;
+let kylaSDNum = 1;
+let kylaSD1;
+let kylaSD2;
+let stanDNum = 1;
+let stanD1;
+let stanD2;
+
+
+let convoStep = 0; 
+
 let map;
 let floor1; 
+let interactNum;
 
+let startButton;
+let exitButton;
+
+// Game Variables
 let gameState = "mainMenu";
 let level = 1;
 
 //Collision Sprites
-let TopWall1;
+let level1Walls;
+let level2Walls;
+let level3Walls;
+
+//NPC Sprites
+let stanNPC;
+let stanNPCXtraCollider;
 
 function preload() 
 {
-  idleAniK = loadAnimation(imageSequence('Sprites/Kyla/Kyla_Idle', 4));
+  idleDownAniK = loadAnimation(imageSequence('Sprites/Kyla/Kyla_Idle', 4));
+  idleUpAniK = loadAnimation(imageSequence('Sprites/Kyla/Kyla_IdleUp', 4));
   walkLAniK = loadAnimation(imageSequence('Sprites/Kyla/Kyla_WalkLeft', 5));
   walkRAniK = loadAnimation(imageSequence('Sprites/Kyla/Kyla_WalkRight', 5));
   walkUAniK = loadAnimation(imageSequence('Sprites/Kyla/Kyla_WalkUp', 4));
   walkDAniK = loadAnimation(imageSequence('Sprites/Kyla/Kyla_WalkDown', 4));
   floor1 = loadImage('Sprites/Map/RCC First Floor.png');
+  stanIdleAni =loadAnimation(imageSequence('Sprites/NPC/Stan/Stan_Idle', 4))
+  dialogueStan = loadImage('Sprites/NPC/Stan/DialogueStan.png');
+  stanD1 = loadImage('Sprites/NPC/Stan/Stan_D_1.png');
+  stanD2 = loadImage('Sprites/NPC/Stan/Stan_D_2.png');
+  dialogueKyla = loadImage('Sprites/Kyla/DialogueKyla.png');
+  kylaSD1 = loadImage('Sprites/Kyla/KylaSD_1.png');
+  kylaSD2 = loadImage('Sprites/Kyla/KylaSD_2.png');
 }
 
 function setup() 
 {
   new Canvas(1528, 842);
-  
-  // Setting Up Player Sprite
+  interactNum = 0;
+
   player = new Sprite();
-  player.addAni('idle', idleAniK);
+  player.addAni('idleDown', idleDownAniK);
   player.addAni('walkLeft', walkLAniK);
   player.addAni('walkRight', walkRAniK);
   player.addAni('walkUp', walkUAniK);
   player.addAni('walkDown', walkDAniK);
+  player.addAni('idleUp', idleUpAniK)
   player.h = 16;
   player.anis.offset.y = -127.5;
+  player.collider = 'dynamic';
   player.rotationLock = true;
-  player.debug = true;
+  player.debug = false;
 
-  // Setting Up Level 1 Collision Sprites
-  TopWall1 = new Sprite(-600, -480, 2000, 200); 
-  TopWall1.collider = 'static';
-  TopWall1.debug = true;
+  stanNPC = new Sprite(1190, -40, 105, 355);
+  stanNPC.addAni('idle', stanIdleAni);
+  stanNPC.ani.frameDelay = 10;
+  stanNPC.collider = 'kinematic';
+  stanNPC.debug = false;
+
+  stanNPCXtraCollider = new Sprite(1190, -50, 105, 345);
+  stanNPCXtraCollider.collider = 'static';
+  stanNPCXtraCollider.visible = false;
+  stanNPCXtraCollider.debug = false;
+
+  level1Walls = new Group();
+  level1Walls.collider = 'static';
+  level1Walls.debug = true;
+  new level1Walls.Sprite(-2967.5, 278, 400, 2675);
+  new level1Walls.Sprite(-1765, -520, 2000, 270);
+  new level1Walls.Sprite(-726.5, -190, 77, 1200);
+  new level1Walls.Sprite(2456, -185, 6288, 550);
+  new level1Walls.Sprite(2670,170, 820, 160);
+  new level1Walls.Sprite(5775,347.5,350,1615);
+  new level1Walls.Sprite(2596,1467.5,6708,625);
+  new level1Walls.Sprite(-1965,1698.5,2410,167);
+  
 
   map = new Sprite(0,0, 9600, 5400);
   map.image = floor1;
@@ -59,7 +118,6 @@ function setup()
   player.layer = 10;
   map.layer = 1;
 
-  // Disabling Automatic Stuff
   allSprites.autoDraw = false;
   allSprites.autoUpdate = false;
   world.autoStep = false;
@@ -68,24 +126,26 @@ function setup()
 function draw() 
 {
   background(255);
-  stroke(0);
   if (gameState === "mainMenu") mainMenu();
   if (gameState === "introScene") introScene();
   if (gameState === "runGame") runGame();
+  fill(0);
   text("X: " + floor(mouse.x) + " Y: " + floor(mouse.y), 20, 30);
 }
 
 function mainMenu() 
 {
   background(255)
-  if (mouse.presses()) {
+  if (mouse.presses()) 
+    {
     gameState = "introScene";
   }
 }
 
 function introScene() 
 {
-  if (frameCount > 120) {
+  if (frameCount > 120) 
+    {
     gameState = "runGame";
   }
 }
@@ -93,33 +153,82 @@ function introScene()
 function runGame() 
 {
   clear();
+  updateLevel();
   mouse.visible = true;
   
-  playerMovement();
+
+  if (convoStep == 0) {
+    playerMovement();
+  }
   world.step();
 
   camera.x = player.x;
   camera.y = player.y;
 
-  if (level == 1) {
+  // Initial trigger
+  if (player.overlapping(stanNPC) && (kb.pressed('e')) && convoStep == 0)
+  {
+    convoStep = 1;
+    dialogueTimer = 20;
+    player.vel.x = 0;
+    player.vel.y = 0;
+    player.ani = 'idleUp'
+  }
+
+  if (level == 1) 
+  {
     map.image = floor1;
   }
 
   camera.on();
-
-
-
   allSprites.update();
   allSprites.draw();
   camera.off();
 
+  handleRelayDialogue();
+}
+
+function handleRelayDialogue() 
+{
+  if (convoStep > 0) 
+    {
+    if (dialogueTimer > 0) dialogueTimer--;
+
+    if (convoStep == 1 || convoStep == 3) 
+    {
+      image(dialogueKyla, 0, 0);
+      if (convoStep == 1) image(kylaSD1, 0, 0);
+      if (convoStep == 3) image(kylaSD2, 0, 0);
+    } 
+    else if (convoStep == 2 || convoStep == 4) 
+    {
+      image(dialogueStan, 0, 0);
+      if (convoStep == 2) image(stanD1, 0, 0);
+      if (convoStep == 4) image(stanD2, 0, 0);
+    }
+
+    if (kb.released('space') && dialogueTimer <= 0) 
+    {
+      convoStep++;
+      dialogueTimer = 20;
+    }
+
+    if (convoStep > 4) 
+    {
+      interactNum += 1;
+      convoStep = 0;
+    }
+  }
 }
 
 function playerMovement() 
 {
   let acceleration = 0.8;
-  let maxSpeed = 6;
+  let maxSpeed = 85;
   let friction = 0.85;
+
+  if (kb.presses('1')) level = 1;
+  if (kb.presses('2')) level = 2;
 
   if (kb.presses('a')) lastXKey = 'a';
   if (kb.presses('d')) lastXKey = 'd';
@@ -131,62 +240,15 @@ function playerMovement()
   if (kb.released('w')) lastYKey = 's';
   if (kb.released('s')) lastYKey = 'w';
 
-  if (!kb.pressing('a') && !kb.pressing('d')) 
-  {
-    playerXDirection = 0;
-  }
-  else if (kb.pressing('a') && kb.pressing('d')) 
-  {
-    if (lastXKey == 'd') 
-    {
-      playerXDirection = 1;
-    } 
-    else 
-    {
-      playerXDirection = -1;
-    }
-  } 
-  else if (kb.pressing('a'))
-  {
-    playerXDirection = -1;
-  } 
-  else if (kb.pressing('d')) 
-  {
-    playerXDirection = 1; 
-  }
+  if (!kb.pressing('a') && !kb.pressing('d')) playerXDirection = 0;
+  else if (kb.pressing('a') && kb.pressing('d')) playerXDirection = (lastXKey == 'd') ? 1 : -1;
+  else if (kb.pressing('a')) playerXDirection = -1;
+  else if (kb.pressing('d')) playerXDirection = 1; 
 
-  if (!kb.pressing('w') && !kb.pressing('s')) 
-  {
-    playerYDirection = 0;
-  }
-  else if (kb.pressing('w') && kb.pressing('s')) 
-  {
-    if (lastYKey == 's') 
-    {
-      playerYDirection = 1;
-    } 
-    else 
-    {
-      playerYDirection = -1;
-    }
-  }
-  else if (kb.pressing('w'))
-  {
-    playerYDirection = -1;
-  }
-  else if (kb.pressing('s')) 
-  {
-    playerYDirection = 1;
-  }
-  
-  if (playerXDirection == 0 && playerYDirection == 0) 
-  {
-    playerIsMoving = false;
-  } 
-  else 
-  {
-    playerIsMoving = true;
-  }
+  if (!kb.pressing('w') && !kb.pressing('s')) playerYDirection = 0;
+  else if (kb.pressing('w') && kb.pressing('s')) playerYDirection = (lastYKey == 's') ? 1 : -1;
+  else if (kb.pressing('w')) playerYDirection = -1;
+  else if (kb.pressing('s')) playerYDirection = 1;
 
   let magnitude = Math.sqrt(playerXDirection ** 2 + playerYDirection ** 2)
 
@@ -199,7 +261,6 @@ function playerMovement()
   player.vel.x += playerXDirection * acceleration;
   player.vel.y += playerYDirection * acceleration;
 
-
   if (playerXDirection == 0) player.vel.x *= friction;
   if (playerYDirection == 0) player.vel.y *= friction;
 
@@ -208,28 +269,51 @@ function playerMovement()
 
   if (playerXDirection == 0 && playerYDirection == 0) 
   {
+    player.ani = 'idleDown';
     player.ani.frameDelay = 10;
-    player.ani = 'idle';
   }
-  else if (playerXDirection <  -0.5)
+  else if (playerXDirection < -0.5) 
   {
-    player.ani.frameDelay = 8;
     player.ani = 'walkLeft';
+    player.ani.frameDelay = 8;
   }
   else if (playerXDirection > 0.5)
   {
-    player.ani.frameDelay = 8;
     player.ani = 'walkRight';
+    player.ani.frameDelay = 8;
   }
-  else if (playerYDirection < -0.5)
+  else if (playerYDirection < -0.5) 
   {
-    player.ani.frameDelay = 10; 
     player.ani = 'walkUp';
+    player.ani.frameDelay = 8;
   }
   else if (playerYDirection > 0.5)
   {
-    player.ani.frameDelay = 10; 
     player.ani = 'walkDown';
+    player.ani.frameDelay = 8;
+  }
+}
+
+function updateClock() 
+{
+
+}
+
+function updateLevel() 
+{
+  if (level == 1) 
+  {
+    level1Walls.visible = true;
+    level1Walls.collider = 'static';
+    stanNPC.visible = true;
+    stanNPC.enabled = true;
+  } 
+  else if (level == 2) 
+  {
+    level1Walls.visible = false;
+    level1Walls.collider = 'none';
+    stanNPC.visible = false;
+    stanNPC.collider = 'none';
   }
 }
 
@@ -242,4 +326,3 @@ function imageSequence(prefix, numFrames, extension=".png")
   }
   return sequence;
 }
-
